cfd566f3b2705ed205bf18bbd594cff8ea130eca
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-05 13:51:46 +0200

    feat introduciendo idiomas

--- main.js
+++ main.js
@@ -6,6 +6,22 @@ const toggleText = document.getElementById('toggle-text')
 const toggleColors = document.getElementById('toggle-colors')
 const rootStyles = document.documentElement.style
 
+const flagsElement = document.getElementById('flags')
+
+const tetxToChange = document.querySelectorAll('[data-section]')
+
+const changeLanguage = async (language) => {
+  const requestJson = await fetch(`./languages/${language}.json`)
+  const texts = await requestJson.json()
+  for (textTochange of tetxsToChange) {
+    console.log(textsTochange)
+  }
+}
+
+flagsElement.addEventListener('click', (e) => {
+  changeLanguage(e.target.parentElement.dataset.language)
+})
+
 toggleTheme.addEventListener('click', () => {
   document.body.classList.toggle('dark')
   if (toggleIcon.src.includes('moon.svg')) {
