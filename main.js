3f099b8f5d068f89633bc4ec1875ebacfa66346c
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-06 15:02:57 +0200

    feat metiendo hobbies y arreglando lo hecho

--- main.js
+++ main.js
@@ -6,26 +6,10 @@ const toggleText = document.getElementById('toggle-text')
 const toggleColors = document.getElementById('toggle-colors')
 const rootStyles = document.documentElement.style
 
-const flagsElement = document.getElementById('flags')
-
-const tetxToChange = document.querySelectorAll('[data-section]')
-
-const changeLanguage = async (language) => {
-  const requestJson = await fetch(`./languages/${language}.json`)
-  const texts = await requestJson.json()
-  for (textTochange of tetxsToChange) {
-    console.log(textsTochange)
-  }
-}
-
-flagsElement.addEventListener('click', (e) => {
-  changeLanguage(e.target.parentElement.dataset.language)
-})
-
 toggleTheme.addEventListener('click', () => {
   document.body.classList.toggle('dark')
   if (toggleIcon.src.includes('moon.svg')) {
-    toggleIcon.src = 'assets/icons.svg/sun.svg'
+    toggleIcon.src = './assets/icons.svg/soleado.png'
     toggleText.textContent = 'Light Mode'
   } else {
     toggleIcon.src = 'assets/icons.svg/moon.svg'
